b028251583610b42f033acc0fc93b37794295a74
Ken Kuroiwa <g2110023ee@gmail.com>
2017-07-12 15:57:53 +0900

    add math function

---
+++ app/src/main/kotlin/com/example/a01020409/myapplication/math.kt
@@ -0,0 +1,18 @@
+package com.example.a01020409.myapplication
+
+/**
+ * Created by 01020409 on 2017/07/12.
+ */
+
+fun add(x:Int, y:Int): Int{
+    return x+y
+}
+fun sub(x:Int, y:Int): Int{
+    return x-y
+}
+fun tim(x:Int, y:Int): Int{
+    return x*y
+}
+fun div(x:Int, y:Int): Int{
+    return x/y
+}
